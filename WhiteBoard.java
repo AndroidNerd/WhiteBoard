2c538e0d2406ad6f8cb2132f7e1c6ebada5bfcd1
AndroidNerd <wuqingpizinie@gmail.com>
2016-11-23 14:47:36 +0800

    Create WhiteBoard.java

---
+++ WhiteBoard.java
@@ -0,0 +1,112 @@
+package com.androidNerd.whiteboard;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Created by androidNerd on 2016/11/23.
+ */
+
+public class WhiteBoard extends SurfaceView implements SurfaceHolder.Callback, Runnable {
+    private SurfaceHolder mSurfaceHolder;
+    private Canvas mCanvas;
+    private boolean startDraw;
+    private Path mPath = new Path();
+    private Paint mPaint = new Paint();
+    private Bitmap mBitmap;
+    private Canvas canvas;
+
+    public WhiteBoard(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView();
+    }
+
+    private void initView() {
+        mSurfaceHolder = getHolder();
+        mSurfaceHolder.addCallback(this);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        this.setKeepScreenOn(true);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        startDraw = true;
+        new Thread(this).start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        startDraw = false;
+    }
+
+    @Override
+    public void run() {
+        while (startDraw) {
+            draw();
+        }
+    }
+
+    private void draw() {
+        try {
+            mBitmap = Bitmap.createBitmap(getMeasuredWidth(), getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+            mCanvas = mSurfaceHolder.lockCanvas();
+            canvas = new Canvas(mBitmap);
+            mCanvas.drawColor(Color.WHITE);
+            mPaint.setStyle(Paint.Style.STROKE);
+            mPaint.setStrokeWidth(5.0f);
+            mPaint.setColor(Color.BLACK);
+            mCanvas.drawPath(mPath, mPaint);
+            canvas.drawPath(mPath, mPaint);
+        } catch (Exception e) {
+        } finally {
+            if (mCanvas != null) {
+                mSurfaceHolder.unlockCanvasAndPost(mCanvas);
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mPath.moveTo(x, y);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                mPath.lineTo(x, y);
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+        return true;
+    }
+
+    public void reset() {
+        mPath.reset();
+    }
+
+    public Bitmap getmBitmap() {
+        return mBitmap;
+    }
+
+    public Drawable getDrawable() {
+        return new BitmapDrawable(mBitmap);
+    }
+}
